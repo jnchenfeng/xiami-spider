2203c4a9b3fd6ac6721804523977e6c1884fb7ac
neal <feng.chen@quvideo.com>
2017-01-03 16:15:50 +0800

    修改解析方法

--- ParseHtml.py
+++ ParseHtml.py
@@ -7,7 +7,7 @@ def getSongIdByAlbumUrl(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
     r = requests.get(albumurl, headers=headers)
     if r.status_code == 200:
-        htmlObject = BeautifulSoup(r.content,"html5lib", from_encoding="utf-8")
+        htmlObject = BeautifulSoup(r.content, from_encoding="utf-8")
         musicTable = htmlObject.find("table",class_="track_list")
         for tr in musicTable.findAll("tr"):
             checkBox = tr.find("input")
