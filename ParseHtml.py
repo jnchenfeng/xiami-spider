52338a4e670145cb87b8d10c70a06f652916bbed
neal <feng.chen@quvideo.com>
2017-01-22 14:18:16 +0800

    修改代码风格，pep8

--- ParseHtml.py
+++ ParseHtml.py
@@ -2,57 +2,71 @@
 from bs4 import BeautifulSoup
 import requests
 
-def getSongIdByAlbumUrl(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        htmlObject = BeautifulSoup(r.content, from_encoding="utf-8")
-        musicTable = htmlObject.find("table",class_="track_list")
-        for tr in musicTable.findAll("tr"):
-            checkBox = tr.find("input")
-            if not checkBox:
-                continue
-            yield checkBox.get("value")
 
-def getGenreUrlList(url):
-    genreUrl = "http://www.xiami.com/genre"
+def get_song_id_by_album_url(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
     r = requests.get(url, headers=headers)
     if r.status_code == 200:
-        htmlObject = BeautifulSoup(r.content,from_encoding="utf-8")
-        sidebar = htmlObject.find(id="sidebar")
-        genreTagList = sidebar.findAll("a")
-        for genereTag in genreTagList:
-            href = genereTag.get("href")
+        html_object = BeautifulSoup(r.content, from_encoding="utf-8")
+        music_table = html_object.find("table",class_="track_list")
+        for tr in music_table.findAll("tr"):
+            check_box = tr.find("input")
+            if not check_box:
+                continue
+            yield check_box.get("value")
+
+
+def get_genre_url_list(url):
+    genre_url = "http://www.xiami.com/genre"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        html_object = BeautifulSoup(r.content,from_encoding="utf-8")
+        sidebar = html_object.find(id="sidebar")
+        genre_tag_list = sidebar.findAll("a")
+        for genere_tag in genre_tag_list:
+            href = genere_tag.get("href")
             if(str(href).encode("utf-8").find("gid")==-1):
                 c={}
-                c["title"] = genereTag.get("title")
+                c["title"] = genere_tag.get("title")
                 c["url"] = href
                 yield c
 
-def getSongIdByGenreUrl(url):
-    #将url中detail替换成songs获取到更多里面的歌曲
+
+def get_song_id_by_genre_url(url):
+    """将url中detail替换成songs获取到更多里面的歌曲"""
     url = str(url).replace("detail","songs")
     url = str(url)+"/page/%s"
     i = 1
-    songIdList=[]
+    song_id_list=[]
     while True:
-        urlTemp = url % str(i)
+        url_temp = url % str(i)
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-        r = requests.get(urlTemp, headers=headers)
+        r = requests.get(url_temp, headers=headers)
+        if r.status_code == 200:
+            html_object = BeautifulSoup(r.content, from_encoding="utf-8")
+            songs_div = html_object.find(id="songs")
+            content_div = songs_div.find("div",class_="content")
+            song_div_list = content_div.findAll("div",class_="songwrapper")
+            if not song_div_list:
+                break
+            for songDiv in song_div_list:
+                song_id_list.append(songDiv.get("data-demoid"))
+        i += 1
+    return song_id_list
+
+
+def get_collect_list_by_key(keyword):
+    i = 0
+    while True:
+        url = "http://www.xiami.com/search/collect/page/{0}?key={1}".format(i, keyword)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+        r = requests.get(url, headers=headers)
         if r.status_code == 200:
             htmlObject = BeautifulSoup(r.content, from_encoding="utf-8")
-            songsDiv = htmlObject.find(id="songs")
-            contentDiv = songsDiv.find("div",class_="content")
-            songDivList = contentDiv.findAll("div",class_="songwrapper")
-            if not songDivList:
-                break
-            for songDiv in songDivList:
-                songIdList.append(songDiv.get("data-demoid"))
-        i = i+1
-    return songIdList
 
 if __name__ == '__main__':
-    genreList = getSongIdByGenreUrl("http://www.xiami.com/genre/detail/sid/3223")
+    genreList = get_song_id_by_genre_url("http://www.xiami.com/genre/detail/sid/3223")
     print genreList
--- xiamispider.py
+++ xiamispider.py
@@ -4,7 +4,7 @@ import ParseHtml as htmlParseProcessor
 
 
 if __name__ == '__main__':
-    songIdList = htmlParseProcessor.getSongIdByAlbumUrl("http://www.xiami.com/album/kjDkae9a9")
+    songIdList = htmlParseProcessor.get_song_id_by_album_url("http://www.xiami.com/album/kjDkae9a9")
     for songid in songIdList:
         url = "http://www.xiami.com/song/" + str(songid)
         print(url)
